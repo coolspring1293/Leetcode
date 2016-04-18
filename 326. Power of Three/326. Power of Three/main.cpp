86eab46570927e91d3e98d714102c10ba6f0a7f3
coolspring1293 <coolspring1293@outlook.com>
2016-04-19 02:50:58 +0800

    Add test

--- 326. Power of Three/326. Power of Three/main.cpp
+++ 326. Power of Three/326. Power of Three/main.cpp
@@ -8,6 +8,9 @@
 
 #include <iostream>
 #include <cmath>
+#include <cfloat>
+#include <stdio.h>
+#include <string>
 /*
  Given an integer, write a function to determine if it is a power of three.
  
@@ -16,18 +19,50 @@
  
 
  */
-
+const double EPSINON = 1E-14;
+char buf[100]={'\0'};
 class Solution {
 public:
     bool isPowerOfThree(int n) {
+        double a = log(n) / log(3);
+        
+        sprintf(buf,"%.100lf",a);
+        std::cout << buf << "\n";
+        std::cout << "org:   "<< a<< "_________\n";
+        std::cout << "int:   "<< int(a)<< "_________\n";
+        std::cout << "floor: "<< floor(a)<< "_________\n";
+        std::cout << "ceil:  "<< ceil(a)<< "_________\n";
+        std::cout << "round:  "<< round(a)<< "_________\n";
+        return (std::abs(a - round(a)) < EPSINON);
+    }
+    
+    bool isPowerOfThree2(int n) {
         double a = log10(n) / log10(3);
-        return (a - int(a) == 0) ? true : false;
+        sprintf(buf,"%.100lf",a);
+        std::cout << buf << "\n";
+        std::cout << "org:   "<< a<< "_________\n";
+        std::cout << "int:   "<< int(a)<< "_________\n";
+        std::cout << "floor: "<< floor(a)<< "_________\n";
+        std::cout << "ceil:  "<< ceil(a)<< "_________\n";
+        std::cout << "round:  "<< round(a)<< "_________\n";
+        return (a - int(a) == 0) ;
     }
 };
 
 int main(int argc, const char * argv[]) {
+    double x = log(3), y = log10(3);
+    
+    sprintf(buf,"%.100lf",x);
+    std::cout << "x = "<< buf << "\n";
+    sprintf(buf,"%.100lf",y);
+    std::cout<< "y = "<< buf << "\n";
+
+    
     Solution s;
-    std::cout << s.isPowerOfThree(27) << "\n";
+    std::cout << s.isPowerOfThree2(14348908) << "\n";
+    std::cout << s.isPowerOfThree(14348908) << "\n";
     std::cout << "Hello, World!\n";
+    
+    std::cout << DBL_MAX << "\t" << DBL_MIN;
     return 0;
 }
