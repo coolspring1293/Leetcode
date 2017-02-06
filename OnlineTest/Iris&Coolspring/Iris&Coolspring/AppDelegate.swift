fbe42394d38224337166ed56adcf77fc28675d95
coolspring1293 <coolspring1293@outlook.com>
2017-02-06 16:39:24 +0800

    add tests

---
+++ OnlineTest/Iris&Coolspring/Iris&Coolspring/AppDelegate.swift
@@ -0,0 +1,77 @@
+//
+//  AppDelegate.swift
+//  Iris&Coolspring
+//
+//  Created by Kieran Will on 11/21/16.
+//  Copyright © 2016 liuw53. All rights reserved.
+//
+
+import Cocoa
+import ServiceManagement
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+    @IBOutlet weak var statusMenu: NSMenu!
+    @IBOutlet weak var loginOnStartupItem: NSMenuItem!
+    var statusItem: NSStatusItem?
+
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+        statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+        statusItem?.menu = statusMenu
+        statusItem?.highlightMode = true
+
+        
+        fetch()
+        NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(AppDelegate.fetch), userInfo: nil, repeats: true)
+    }
+
+    
+    
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+    
+    func fetch() {
+        let date = NSDate()
+        
+        let timeFormatter = NSDateFormatter()
+        timeFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let dateAsString = "2015-09-15 23:00:00"
+        let date1 = timeFormatter.dateFromString(dateAsString)!
+        
+        let timeFormatter2 = NSDateFormatter()
+        timeFormatter2.dateFormat = "HH:mm:ss"
+        let strNowTime = timeFormatter2.stringFromDate(date) as String
+        self.statusItem?.title = strNowTime
+        
+        
+        let diffDateComponents = NSCalendar.currentCalendar().components([
+//            NSCalendarUnit.Year,
+//            NSCalendarUnit.Month,
+            NSCalendarUnit.Day,
+            NSCalendarUnit.Hour,
+            NSCalendarUnit.Minute,
+            NSCalendarUnit.Second],
+            fromDate: date1,
+            toDate: date,
+            options: NSCalendarOptions.init(rawValue: 0))
+        
+        let strNowTime2 = "❤️Love you for \(diffDateComponents.day) d, \(diffDateComponents.hour) h, \(diffDateComponents.minute) min, \(diffDateComponents.second) s";
+        
+        self.statusItem?.title = strNowTime2
+    }
+    
+
+    
+    @IBAction func quit(menuItem: NSMenuItem) {
+        NSApplication.sharedApplication().terminate(self)
+    }
+    
+
+}
+
---
+++ OnlineTest/Iris&Coolspring/Iris&Coolspring/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Iris&Coolspring
+//
+//  Created by Kieran Will on 11/21/16.
+//  Copyright © 2016 liuw53. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
